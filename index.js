e62f4a515e343ae147879de2a6b61db083c250ed
Sarfaroj Ansari <sarfarosha66@gmail.com>
2024-05-31 22:15:29 +0530

    Refactor routes with express framework

--- index.js
+++ index.js
@@ -1,47 +1,67 @@
-const http = require("http");
-const fs = require("fs");
+// const http = require("http");
+// const fs = require("fs");
+// const path = require("path");
+// const PORT = process.env.PORT || 8080;
+
+// const server = http.createServer((req, res) => {
+//   res.setHeader("Content-Type", "text/html");
+
+//   let filePath = "./";
+//   switch (req.url) {
+//     case "/":
+//       filePath += "index.html";
+//       res.statusCode = 200;
+//       break;
+//     case "/about":
+//       filePath += "about.html";
+//       res.statusCode = 200;
+//       break;
+//     case "/contact":
+//       filePath += "contact-me.html";
+//       res.statusCode = 200;
+//       break;
+//     case "/contact-me":
+//       res.statusCode = 301;
+//       res.setHeader("Location", "/contact");
+//       res.end();
+//       break;
+//     default:
+//       filePath += "404.html";
+//       res.statusCode = 404;
+//       break;
+//   }
+
+//   fs.readFile(path.join(__dirname, filePath), (err, data) => {
+//     if (err) {
+//       console.log(err);
+//       res.end();
+//     } else {
+//       res.write(data);
+//       res.end();
+//     }
+//   });
+// });
+
+// server.listen(PORT, () => {
+//   console.log("Server Running");
+// });
+
+const express = require("express");
+const app = express();
+const port = 3000;
 const path = require("path");
-const PORT = process.env.PORT || 8080;
 
-const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/html");
-
-  let filePath = "./";
-  switch (req.url) {
-    case "/":
-      filePath += "index.html";
-      res.statusCode = 200;
-      break;
-    case "/about":
-      filePath += "about.html";
-      res.statusCode = 200;
-      break;
-    case "/contact":
-      filePath += "contact-me.html";
-      res.statusCode = 200;
-      break;
-    case "/contact-me":
-      res.statusCode = 301;
-      res.setHeader("Location", "/contact");
-      res.end();
-      break;
-    default:
-      filePath += "404.html";
-      res.statusCode = 404;
-      break;
-  }
-
-  fs.readFile(path.join(__dirname, filePath), (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end();
-    } else {
-      res.write(data);
-      res.end();
-    }
-  });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
-server.listen(PORT, () => {
-  console.log("Server Running");
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "/about.html"));
 });
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "/contact-me.html"));
+});
+
+app.listen(port);
+console.log(`Server started on http://localhost:${port}`);
